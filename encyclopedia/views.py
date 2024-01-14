f9b0be09ad7b14ff31a8e929ae7b1a208af20aac
Ali <ali.zolghadr@gmail.com>
2024-01-15 10:24:10 +1100

    Add entry page functionality - use markdown2 to convert markdown to HTML

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
 from . import util
+import markdown2
+
+markdowner = markdown2.Markdown()
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title": title.upper(),
+        "content": markdowner.convert(util.get_entry(title.upper()))
     })
 
