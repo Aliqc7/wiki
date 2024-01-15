f30e42a07879452db1624eb4331adbdc2342bdc1
Ali <ali.zolghadr@gmail.com>
2024-01-15 16:01:18 +1100

    Add case-sensitive search functionality

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -19,3 +19,27 @@ def entry(request, title):
         "content": content
     })
 
+def search(request):
+    query = request.GET.get('q', '')
+    entry_list = util.list_entries()
+
+    if query in entry_list:
+        content = util.get_entry(query)
+        content = markdowner.convert(content)
+        return render(request, "encyclopedia/entry.html", {
+        "content": content
+    })
+    else:
+
+
+        
+        return render(request, "encyclopedia/search.html", {
+            "query": query,
+            "entry_list": entry_list
+        })
+
+
+
+
+
+
